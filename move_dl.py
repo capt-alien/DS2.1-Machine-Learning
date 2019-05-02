2870074a119989b15bf7c57993a5828087ef93c2
capt-alien <eric.botcher@presidio.edu>
2019-05-02 16:11:23 -0700

    something

---
+++ move_dl.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+"""this script, when run, will move the most recent download from
+    my downloads folder to the current working directory or the
+    specified folder"""
+
+def move_dl():
+    pass
+
+def rename():
+    pass
