ed74befb3a8a31d5c19daf552fd9a68a1264125e
capt-alien <eric.botcher@presidio.edu>
2019-05-16 15:58:37 -0700

    almost finished

---
+++ lyft_stock/apply_model_1.py
@@ -0,0 +1,13 @@
+import quandl
+
+# import numpy
+
+# a = numpy.array([1, 2, 3])
+
+import quandl, math
+import numpy as np
+import pandas as pd
+# from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+
+df = quandl.get("WIKI/GOOGL")
